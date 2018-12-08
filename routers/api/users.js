161e4450efa946e66ffb010231cc20a94696ee9c
成雨 <rensailong@zhongan.io>
2018-12-08 14:35:01 +0800

    登录

--- routers/api/users.js
+++ routers/api/users.js
@@ -2,7 +2,7 @@
  * @Author: 成雨
  * @Date: 2018-12-08 13:12:19 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-08 14:09:49
+ * @Last Modified time: 2018-12-08 14:34:22
  */
 
 const express = require('express');
@@ -25,18 +25,26 @@ router.get('/test', (req, res) => {
 
 /**
  * $route POST api/users/register
- * @desc 返回亲求的json数据
+ * @desc 注册
  * @access public
  */
 router.post('/register', (req, res) => {
     // 查询数据库中是否有邮箱
 
-    User.findOne({email:req.body.email})
+    User.findOne({
+            email: req.body.email
+        })
         .then((user) => {
             if (user) {
-                return res.status(400).json({email: '邮箱已被注册'})
+                return res.status(400).json({
+                    email: '邮箱已被注册'
+                })
             } else {
-                var avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'});
+                var avatar = gravatar.url(req.body.email, {
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm'
+                });
 
                 const newUser = new User({
                     name: req.body.name,
@@ -44,10 +52,10 @@ router.post('/register', (req, res) => {
                     password: req.body.password,
                     avatar,
                 });
-                
+
                 // 加密密码
-                bcrypt.genSalt(10, function(err, salt) {
-                    bcrypt.hash(newUser.password, salt, function(err, hash) {
+                bcrypt.genSalt(10, function (err, salt) {
+                    bcrypt.hash(newUser.password, salt, function (err, hash) {
                         // Store hash in your password DB.
                         if (err) throw err;
 
@@ -60,6 +68,43 @@ router.post('/register', (req, res) => {
                 });
             }
         });
+});
+
+/**
+ * $route POST api/users/login
+ * @desc 登陆 返回token jwt passport
+ * @access public
+ */
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // 查询数据库
+    User.findOne({
+            email
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    email: '用户不存在！',
+                });
+            }
+
+            // 密码匹配
+            // Load hash from your password DB.
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({
+                            msg: 'success'
+                        });
+                    } else {
+                        return res.status(400).json({
+                            password: '密码错误！'
+                        })
+                    }
+                });
+        })
 });
 
 module.exports = router;
