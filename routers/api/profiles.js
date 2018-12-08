595971bb89cce843eee913f67ffeef6abbfab08a
成雨 <rensailong@zhongan.io>
2018-12-08 22:28:13 +0800

    查询

--- routers/api/profiles.js
+++ routers/api/profiles.js
@@ -2,7 +2,7 @@
  * @Author: 成雨 
  * @Date: 2018-12-08 17:06:10 
  * @Last Modified by: 成雨
- * @Last Modified time: 2018-12-08 17:37:32
+ * @Last Modified time: 2018-12-08 22:25:12
  */
 
 const express = require('express');
@@ -32,10 +32,51 @@ router.post('/add', passport.authenticate('jwt', {
         .then((result) => {
             res.json(result);
         }).catch((err) => {
-            
+            console.log(err);
         });
 });
 
+
+/**
+ * $route POST api/profiles
+ * @desc 获取所有信息
+ * @access Pravite
+ */
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    Profiles.find()
+        .then((result) => {
+            if (!result) {
+                return res.status(4040).json('没有任何内容');
+            }
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
+
+/**
+ * $route POST api/profiles
+ * @desc 获取单个信息
+ * @access Pravite
+ */
+router.get('/:id', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    Profiles.findOne({
+            _id: req.params.id
+        })
+        .then((result) => {
+            if (!result) {
+                return res.status(4040).json('没有任何内容');
+            }
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+        });
+});
 
 
 module.exports = router;
