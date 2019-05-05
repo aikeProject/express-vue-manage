1cd6afae40e9ac0c9b0fa0179a77f3d8bf816069
成雨 <rensailong@zhongan.io>
2019-05-05 18:23:44 +0800

    部署pm2

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ RUN npm i
 # 暴露3000端口
 EXPOSE 3000
 
-CMD npm start
+CMD npm publish
---
+++ app.js
@@ -0,0 +1,76 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const log4js = require('./config/log4js');
+const errLogger = log4js.getLogger('err');
+const othLogger = log4js.getLogger('oth');
+// token认证
+const passport = require('passport');
+
+// api users.js
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+
+// DB config
+// const db = require('./config/keys').local.mongoURL;
+const db = require('./config/keys').server.mongoURL;
+
+// Connect to mongodb
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => {
+      othLogger.info('连接 Mongodb 成功');
+    })
+    .catch(err => errLogger.error(err));
+
+const app = express();
+
+// 使用中间件实现允许跨域
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     next();
+// });
+
+// 请求日志
+// log4js.useLogger(app, logger);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 认证处理
+require('./config/passport')(passport);
+
+// passport init初始化
+app.use(passport.initialize());
+
+// api
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
